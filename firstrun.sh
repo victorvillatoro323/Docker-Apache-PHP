4996ff5e45911fa9c0aef0faf8577ce0707d45ea
hernandito <archi@inch.com>
2015-05-02 16:50:37 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -20,13 +20,13 @@ fi
 # Add Persistent Cron Configuration Capability
 if [ -f /config/crons.conf ]; then
   echo "Using existing Cron config file."
-  crontab /config/crons.conf
-  cron
+#  crontab /config/crons.conf
+#  cron
 else
   echo "Copying blank  Cron config file."
   cp /root/crons.conf /config/crons.conf
-  crontab /config/crons.conf
-  cron
+#  crontab /config/crons.conf
+#  cron
 fi
 
 
